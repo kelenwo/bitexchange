4f4708560653f666ca60520d34fbe20e7724401e
kelvin sydney <kelenwo68@gmail.com>
2023-10-24 16:34:57 +0100

    Added login and register

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -7,6 +7,11 @@ use Illuminate\View\View;
 
 class dashboardController extends Controller
 {
+
+    public function about(): View
+    {
+        return view('about');
+    }
     public function index(): View
     {
         return view('dashboard.index');
@@ -43,15 +48,5 @@ class dashboardController extends Controller
     public function account_security(): View
     {
         return view('dashboard.account_security');
-    }
-
-    public function login(): View
-    {
-        return view('dashboard.login');
-    }
-
-    public function register(): View
-    {
-        return view('dashboard.register');
     }
 }
