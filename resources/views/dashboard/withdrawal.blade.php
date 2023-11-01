b5d9a012713acaed12b20cd150f8e89dd12d92dc
kelvin sydney <kelenwo68@gmail.com>
2023-11-01 16:56:57 +0100

    Made changes to withdrawal

--- resources/views/dashboard/withdrawal.blade.php
+++ resources/views/dashboard/withdrawal.blade.php
@@ -1,14 +1,7 @@
 @extends('layouts.dashboard')
 @section('title', 'Dashboard')
 @section('content')
-    <style>
-        img {
-            border: 1px #dddddd solid !important;
-            border-radius: 0 !important;
-            height: 16px !important;
-            width: 38px !important;
-        }
-    </style>
+
     <div class="content-wrapper">
         <div class="page-header">
             <h3 class="page-title"> Withdrawal </h3>
@@ -19,81 +12,97 @@
                 </ol>
             </nav>
         </div>
-        <div class="container-fluid">
-            <h6>Ask for withdrawal:</h6>
-            <br>
-            <form method="post">
-                <input type="hidden" name="form_id" value="16979583406945"><input type="hidden" name="form_token" value="8ea231c8b719854e28b5fda9aef8941b">
-                <input type="hidden" name="a" value="withdraw">
-                <input type="hidden" name="action" value="preview">
-                <input type="hidden" name="say" value="">
-                <div class="table-responsive">
-                    <table class="table table-borderless table-hover table-centered m-0">
-                        <tbody>
-                        <tr>
-                            <td>Account Balance:</td>
-                            <td>$<b>0.00</b></td>
-                        </tr>
-                        <tr>
-                            <td>Pending Withdrawals: </td>
-                            <td>$<b></b></td>
-                        </tr>
-                        </tbody>
-                    </table>
+
+        <div class="row">
+            <div class="col-md-7 grid-margin stretch-card">
+                <div class="card">
+                    <form method="POST" action="{{ route('deposit') }}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <h5 class="">Ask for withdrawal</h5>
+
+                            @if(Session::has('success'))
+                                <div class="py-2 alert alert-success" role="alert">
+                                    {{ Session::get('success') }}
+                                </div>
+                            @endif
+                            @if(Session::has('error'))
+                                <div class="py-2 alert alert-danger" role="alert">
+                                    {{ Session::get('error') }}
+                                </div>
+                            @endif
+
+                            <div class="form-group mt-3">
+                                <label for="exampleInputUsername1">Enter Amount to Withdraw</label>
+                                <input type="number" name="amount" class="form-control form-control-sm" value="50.0" placeholder="Withdrawal Amount" step="1">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputUsername1">Select Payment Method</label>
+                                <select class="form-control form-control-sm" name="method">
+                                    <option value="usdt">USDT (trc20)</option>
+                                    <option value="btc">Bitcoin</option>
+                                    <option value="eth">Ethereum</option>
+                                    <option value="bnb">BNB (Bep20)</option>
+                                    <option value="trx">TRX</option>
+                                </select>
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="exampleInputUsername1">Wallet Address</label>
+                                <input type="text" name="wallet" class="form-control form-control-sm" value="No wallet set" readonly>
+                                <p class="my-2">
+                                    <a href="{{ route('account') }}" >Set your wallet address</a>
+                                </p>
+                            </div>
+                            <button type="button" class="btn btn-primary btn-sm mr-2" data-toggle="modal" data-target="#depositModal">Submit</button>
+                        </div>
+
+                        <div class="modal fade" id="depositModal" tabindex="-1" role="dialog" aria-labelledby="depositModal" aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header card m-0">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body mt-0 card">
+                                        <div class="">
+                                            <div class="">
+                                                <div class="row">
+                                                    <div class="col-md-8">
+                                                        <p class="card-description mb-2">
+                                                            Please pay the equivalent amount in ethereum, provide the transaction ID and receipt screenshot(optional)
+                                                        </p>
+                                                        <span>Address: </span>
+                                                        <h6>0x782405D5F3Dc89993f19fC0cbA0d5e20172cf7eb</h6>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <img src="{{ asset('images/qrcode.jpg') }}" height="150" width="150"/>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group border-top pt-4">
+                                                        <label for="transaction_id">Transaction ID <span class="text-danger">*</span> </label>
+                                                        <input type="text" name="transaction_id" id="transaction_id" class="form-control form-control-sm" placeholder="Transaction ID" step="1">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label for="receipt">Receipt</label>
+                                                        <input type="file" accept="image/*" name="receipt" id="receipt" class="form-control ">
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <button type="submit" class="btn btn-primary btn-sm mr-2">Submit</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="table-responsive">
-                    <table class="table table-borderless table-hover table-centered m-0">
-                        <thead class="thead-light">
-                        <tr>
-                            <th></th>
-                            <th>Processing</th>
-                            <th>Available</th>
-                            <th>Pending</th>
-                            <th>Account</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td></td>
-                            <td><img src="{{asset('images/dashboard/1000.gif')}}" width="44" height="17" align="absmiddle"> Usdt (trc20)</td>
-                            <td><b style="color:green">$0.00</b></td>
-                            <td><b style="color:red">$0.00</b></td>
-                            <td><span class="badge bg-soft-primary text-danger"><a href="?a=edit_account"><i>not set</i></a></span></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><img src="{{asset('images/dashboard/1001.gif')}}" width="44" height="17" align="absmiddle"> Bitcoin</td>
-                            <td><b style="color:green">$0.00</b></td>
-                            <td><b style="color:red">$0.00</b></td>
-                            <td><span class="badge bg-soft-primary text-danger"><a href="?a=edit_account"><i>not set</i></a></span></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><img src="{{asset('images/dashboard/1002.gif')}}" width="44" height="17" align="absmiddle"> Ethereum</td>
-                            <td><b style="color:green">$0.00</b></td>
-                            <td><b style="color:red">$0.00</b></td>
-                            <td><span class="badge bg-soft-primary text-danger"><a href="?a=edit_account"><i>not set</i></a></span></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><img src="{{asset('images/dashboard/1003.gif')}}" width="44" height="17" align="absmiddle"> BNB (bep20)</td>
-                            <td><b style="color:green">$0.00</b></td>
-                            <td><b style="color:red">$0.00</b></td>
-                            <td><span class="badge bg-soft-primary text-danger"><a href="?a=edit_account"><i>not set</i></a></span></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><img src="{{asset('images/dashboard/1004.gif')}}" width="44" height="17" align="absmiddle"> TRX</td>
-                            <td><b style="color:green">$0.00</b></td>
-                            <td><b style="color:red">$0.00</b></td>
-                            <td><span class="badge bg-soft-primary text-danger"><a href="?a=edit_account"><i>not set</i></a></span></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <br><br>
-                <div class="alert alert-danger" role="alert">You have no funds to withdraw.</div>
-            </form>
+            </div>
+            <!-- Modal -->
         </div>
+
     </div>
 @endsection
